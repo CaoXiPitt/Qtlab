c13520ca58a0a127761ccc5c956a2b846df2dd10
ErickBrindock <esb43@pitt.edu>
2016-06-16 18:37:05 -0400

    trace display works get does not

--- qtlab-master/instrument_plugins/Keysight_MXA_N9020A.py
+++ qtlab-master/instrument_plugins/Keysight_MXA_N9020A.py
@@ -237,9 +237,9 @@ class Keysight_MXA_N9020A(Instrument):
     def do_set_comm(self, address):
         self._visainstrument.write(':SYST:COMM:SOUR[1]:ADDR "%s"' %address)
     def do_get_trace(self, channel):
-        return self._visainstrument.ask('TRAC[%s]:DISP?' %channel)
-    def do_set_trace(self, channel, enable):
-        self._visainstrument.write('TRAC[{}]:DISP {}'.format(channel, enable))
+        return self._visainstrument.ask('TRAC%s:DISP?' %channel)
+    def do_set_trace(self, enable):
+        self._visainstrument.write('TRAC1:DISP %s' % enable)
     def get_all(self):
         self.get_frequency_center()
         self.get_frequency_start()
