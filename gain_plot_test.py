c809b17cd5728be1cc9c6bb5af785412d180e0d8
ErickBrindock <esb43@pitt.edu>
2016-07-07 11:39:42 -0400

    Gain_Plot_Test  completed
    gain plot test now works. Loads from h5py file, but does not rely on it

--- gain_plot_test.py
+++ gain_plot_test.py
@@ -10,45 +10,49 @@ from matplotlib.widgets import Slider
 import h5py
 import numpy as np
 
+# Load Data from h5py File
 fp = h5py.File('C:\\Qtlab\\gain_sweep_data\\JPC_pump_sweep_7_6_2016_15', 'r')
-#print fp['trace_data_]
-test_data = np.array([[1,2,3], [1,2,3]])
-#plt.plot(test_data[0], test_data[1])
 
+sweep_freqs = fp['frequencies'][:]
+sweep_freqs = sweep_freqs.tolist()
+sweep_powers = fp['powers'][:]
+sweep_powers = sweep_powers.tolist()
+for i in range(len(sweep_powers)):  # Remove floating point error
+    sweep_powers[i] = int(sweep_powers[i]*10)/10.0
+gain = []
+for fi in range(len(sweep_freqs)):
+    for pi in range(len(sweep_powers)):
+        gain.append(fp['trace_data_{}_{}'.format(sweep_powers[pi], sweep_freqs[fi])][0])
+measurement_frequency = fp['fdata_{}_{}'.format(sweep_powers[0], sweep_freqs[0])][:]
 
-freqs = fp['frequencies'][:]
-pows = fp['powers'][:]
-freq_index =int(len(freqs)/2)
-#TODO get array of gains from fp file
-gain = fp['trace_data_{}_{}'.format(pows[0], freqs[freq_index])][0]
-mfrequency = fp['fdata_{}_{}'.format(pows[0], freqs[freq_index])]
-
-
-
-data_plot, = plt.plot(mfrequency, gain)
-plt.axis([mfrequency[0], mfrequency[-1], -5, 30])
-ax_power = plt.axes([0.2,0.01, 0.65, 0.03])
-sl_power = Slider(ax_power, 'Power', 0, 20, valinit=0)
-print pows.tolist()
-def update(val):
-    #TODO figure out method to update slider
-    val = int(val)
-    index = int(val)
-    print index
-    data_plot.set_ydata(gain[index])
-    if (sl_power.val != pows[index]):
-        sl_power.set_val(pows[index])
-        
-sl_power.on_changed(update)
-plt.show()
-#for i in range(22):
-#    freq_index =int(len(freqs)/2)]
-#    gain = fp['trace_data_{}_{}'.format(pows[i], freqs[freq_index])][0]
-#    mfrequency = fp['fdata_{}_{}'.format(pows[i], freqs[freq_index])]
-#    plt.plot(mfrequency, gain)
-#    plt.xlabel('frequency(Hz)')
-#    plt.ylabel('gain(dB)')
-#    plt.ylim((-5, 30))
-#    plt.title('Plot at {} {}'.format(pows[i],freqs[freq_index]))
-#    plt.show()
 fp.close()
+
+# Main Data Plot Setup
+data_plot, = plt.plot(measurement_frequency, gain[10])
+plt.axis([measurement_frequency[0], measurement_frequency[-1], -5, 30])
+plt.gcf().subplots_adjust(left = .05, right = .95,top = .95, bottom = .15)
+plt.title('Power and Frequency Sweep')
+plt.xlabel('Frequency (Ghz)')
+plt.ylabel('Gain (dBm)')
+
+# Slider Setup
+freq_axes = plt.axes([0.2, 0.01, 0.65, 0.03])
+freq_slider = Slider(freq_axes, 'Frequency', min(sweep_freqs), max(sweep_freqs), valinit=min(sweep_freqs), valfmt = '%.0f Hz')
+power_axes = plt.axes([0.2, 0.05, 0.65, 0.03])
+power_slider = Slider(power_axes, 'Power', min(sweep_powers), max(sweep_powers), valinit=min(sweep_powers), valfmt = '%.1f dB')
+
+def update(val):
+    sval = int(power_slider.val*10)/10.0
+    fval = freq_slider.val
+    findex = sweep_freqs.index(min(sweep_freqs, key=lambda x:abs(x-fval)))
+    pindex = sweep_powers.index(min(sweep_powers, key=lambda x:abs(x-sval)))
+    index = pindex + (findex*len(sweep_powers))    
+    data_plot.set_ydata(gain[index])
+    if (power_slider.val != sweep_powers[pindex]):
+        power_slider.set_val(sweep_powers[pindex])
+    if (freq_slider.val != sweep_freqs[findex]):
+        freq_slider.set_val(sweep_freqs[findex])
+        
+power_slider.on_changed(update)
+freq_slider.on_changed(update)
+plt.show()
