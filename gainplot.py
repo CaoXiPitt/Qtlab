3b0d883748da5702043a43220cb0b3c24cb05cad
alex-rowden <alex.rowden@pitt.edu>
2016-07-11 15:51:02 -0400

    Changes to data structure

--- gainplot.py
+++ gainplot.py
@@ -56,21 +56,49 @@ class GainSweepPlot(object):
         self.freq_slider.on_changed(self.update)
         plt.show()
         
+#    def update(self, val):
+#        sval = int(self.power_slider.val*10)/10.0
+#        fval = self.freq_slider.val
+#        findex = self.sweep_freqs.index(min(self.sweep_freqs, key=lambda x:abs(x-fval)))
+#        pindex = self.sweep_powers.index(min(self.sweep_powers, key=lambda x:abs(x-sval)))
+#        index = pindex + (findex*len(self.sweep_powers))    
+#        self.data_plot.set_ydata(self.gain[index])
+#        if (self.power_slider.val != self.sweep_powers[pindex]):
+#            self.power_slider.set_val(self.sweep_powers[pindex])
+#        if (self.freq_slider.val != self.sweep_freqs[findex]):
+#            self.freq_slider.set_val(self.sweep_freqs[findex])
+            
+#    power_slider.on_changed(update)
+#    freq_slider.on_changed(update)
+#    plt.show()
+#    def load_data_from_file(self, 
+#            filename='C:\\Qtlab\\gain_sweep_data\\JPC_pump_sweep_7_6_2016_15'):
+#        infile = h5py.File(filename, 'a')
+#        self.sweep_freqs = infile['frequencies'][:]
+#        self.sweep_freqs = self.sweep_freqs.tolist()
+#        self.sweep_powers = infile['powers'][:]
+#        self.sweep_powers = self.sweep_powers.tolist()
+#        for i in range(len(self.sweep_powers)):  # Remove floating point error
+#            self.sweep_powers[i] = int(self.sweep_powers[i]*10)/10.0
+#        self.gain = []
+#        for fi in range(len(self.sweep_freqs)):
+#            for pi in range(len(self.sweep_powers)):
+#                self.gain.append(infile['trace_data_{}_{}'.format(self.sweep_powers[pi], self.sweep_freqs[fi])][0])
+#        self.measurement_frequency = infile['fdata_{}_{}'.format(self.sweep_powers[0], self.sweep_freqs[0])][:]
+#        infile.close()
+            
     def update(self, val):
         sval = int(self.power_slider.val*10)/10.0
         fval = self.freq_slider.val
         findex = self.sweep_freqs.index(min(self.sweep_freqs, key=lambda x:abs(x-fval)))
         pindex = self.sweep_powers.index(min(self.sweep_powers, key=lambda x:abs(x-sval)))
-        index = pindex + (findex*len(self.sweep_powers))    
-        self.data_plot.set_ydata(self.gain[index])
+        #index = pindex + (findex*len(self.sweep_powers))    
+        self.data_plot.set_ydata(self.gain[findex][pindex])
         if (self.power_slider.val != self.sweep_powers[pindex]):
             self.power_slider.set_val(self.sweep_powers[pindex])
         if (self.freq_slider.val != self.sweep_freqs[findex]):
             self.freq_slider.set_val(self.sweep_freqs[findex])
             
-#    power_slider.on_changed(update)
-#    freq_slider.on_changed(update)
-#    plt.show()
     def load_data_from_file(self, 
             filename='C:\\Qtlab\\gain_sweep_data\\JPC_pump_sweep_7_6_2016_15'):
         infile = h5py.File(filename, 'a')
@@ -80,9 +108,9 @@ class GainSweepPlot(object):
         self.sweep_powers = self.sweep_powers.tolist()
         for i in range(len(self.sweep_powers)):  # Remove floating point error
             self.sweep_powers[i] = int(self.sweep_powers[i]*10)/10.0
-        self.gain = []
-        for fi in range(len(self.sweep_freqs)):
-            for pi in range(len(self.sweep_powers)):
-                self.gain.append(infile['trace_data_{}_{}'.format(self.sweep_powers[pi], self.sweep_freqs[fi])][0])
+        self.gain = np.array(infile['sweep_data'])
+#        for fi in range(len(self.sweep_freqs)):
+#            for pi in range(len(self.sweep_powers)):
+#                self.gain.append(infile['trace_data_{}_{}'.format(self.sweep_powers[pi], self.sweep_freqs[fi])][0])
         self.measurement_frequency = infile['fdata_{}_{}'.format(self.sweep_powers[0], self.sweep_freqs[0])][:]
         infile.close()
